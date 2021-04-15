bacebbaa99e5c27f97f9a27b6542446659a04952
Cherry <duncanlinnaeus98@gmail.com>
2021-04-15 11:46:50 +0300

    added a firebase

---
+++ app/src/main/java/com/example/recipeapp/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.recipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3*1000);
+    }
+}
